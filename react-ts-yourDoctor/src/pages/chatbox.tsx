0abcfb1c9b151f3489f2c4d7eb0157e322e396d0
Ayesha Athoi <ayeshaathoi62@gmail.com>
2023-08-18 16:07:35 +0600

    not a good chatbox- need upgrade

---
+++ react-ts-yourDoctor/src/pages/chatbox.tsx
@@ -0,0 +1,69 @@
+import { useState, ChangeEvent, FormEvent } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Header from '../navbar/header';
+import Footer from '../navbar/footer';
+
+
+function Chatbox() {
+  const [formData, setFormData] = useState({
+    msg_content: '',
+    msg_time: '',
+    msg_date: '',
+    msg_sender: '',
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await axios
+        .post('http://localhost:3000/comment/get/bookingid', formData)
+        .then((res) => {
+          console.log('here is the form', res.data);
+          console.log(res.data[0].msg_content)
+        //   navigate('/LogIn');
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+
+      <div className="fit-content p-20 ">
+        <p className="text-gray-400">Patient Address</p>
+        <p className="text-gray-400 justify-right">Patient Name</p>
+        <hr className="line-below-text my-4 border-t-2 border-red-300" />
+      </div>
+
+      <div className="flex px-20 pt-10">
+            <div className="w-half rounded-md rounded-lg bg-gray-200 px-3 py-2 ">
+                <div className="mb-8 mt-4">
+                    <p className="text-sm font-bold text-indigo-500">msg sort by time</p>
+                </div>
+                  </div>
+                </div>
+
+
+
+
+      <div className="mt-16">
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default Chatbox;

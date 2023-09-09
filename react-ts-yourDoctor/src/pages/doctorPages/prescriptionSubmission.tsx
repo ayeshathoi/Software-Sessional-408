1d430f24855fb227981821d931b5f9bc07a572c1
Ayesha Athoi <ayeshaathoi62@gmail.com>
2023-09-10 03:30:39 +0600

    quite done 2

--- react-ts-yourDoctor/src/pages/doctorPages/prescriptionSubmission.tsx
+++ react-ts-yourDoctor/src/pages/doctorPages/prescriptionSubmission.tsx
@@ -7,9 +7,9 @@ import Footer from '../navbar/footer';
 // Define the CSS styles as JavaScript objects
 const styles = {
   prescriptionContainer: {
-    maxWidth: '600px',
+    maxWidth: '800px',
     margin: '0 auto',
-    padding: '100px',
+    padding: '200px',
     border: '1px solid #ccc',
     borderRadius: '5px',
   },
@@ -89,6 +89,7 @@ function Prescription() {
       disease: formData.disease,
       tests: formData.tests,
       suggestions: formData.suggestions,
+      medicine: formData.medicine,
     };
     if (noPrescriptionFound === true) {
       // Send a POST request to the backend to create the prescription
@@ -112,54 +113,119 @@ function Prescription() {
       <h2 style={styles.prescriptionHeader}>Create Prescription</h2>
       <form onSubmit={handleSubmit}>
         <div style={styles.formGroup}>
-          <label style={styles.label} htmlFor="disease">
-            Disease:
-          </label>
-          <input
+          {/* <input
             style={styles.input}
             type="text"
             id="disease"
             name="disease"
             value={formData.disease}
             onChange={handleChange}
-          />
+          /> */}
+          <select
+            style={styles.input}
+            id="disease"
+            name="disease"
+            value={formData.disease}
+            onChange={handleChange}
+          >
+            <option value="">Select Disease</option>
+            <option value="Fever">Fever</option>
+            <option value="Headache">Headache</option>
+            <option value="Cough">Cough</option>
+            <option value="Cold">Cold</option>
+            <option value="Diarrhea">Diarrhea</option>
+            <option value="Vomiting">Vomiting</option>
+            <option value="Stomachache">Stomachache</option>
+            <option value="Others">Others</option>
+          </select>
         </div>
         <div style={styles.formGroup}>
-          <label style={styles.label} htmlFor="tests">
-            Tests:
-          </label>
-          <input
+          
+          {/* <input
             style={styles.input}
             type="text"
             id="tests"
             name="tests"
             value={formData.tests}
             onChange={handleChange}
-          />
+          /> */}
+          <select
+            style={styles.input}
+            id="tests"
+            name="tests"
+            value={formData.tests}
+            onChange={handleChange}
+          >
+            <option value="">Select Tests</option>
+            <option value="Blood Test">Blood Test</option>
+            <option value="Urine Test">Urine Test</option>
+            <option value="X-Ray">X-Ray</option>
+            <option value="CT Scan">CT Scan</option>
+            <option value="MRI">MRI</option>
+            <option value="ECG">ECG</option>
+            <option value="Others">Others</option>
+          </select>
         </div>
         <div style={styles.formGroup}>
-          <label style={styles.label} htmlFor="suggestions">
-            Suggestions:
-          </label>
-          <textarea
+          <select
             style={styles.input}
             id="suggestions"
             name="suggestions"
             value={formData.suggestions}
             onChange={handleChange}
-          />
+          >
+            <option value="">Select Suggestions</option>
+            <option value="Take rest">Take rest</option>
+            <option value="Drink plenty of water">Drink plenty of water</option>
+            <option value="Take medicine">Take medicine</option>
+            <option value="Take medicine after meal">
+              Take medicine after meal
+            </option>
+            <option value="Take medicine before meal">
+              Take medicine before meal
+            </option>
+            <option value="Take medicine twice a day">
+              Take medicine twice a day
+            </option>
+            <option value="Take medicine thrice a day">
+              Take medicine thrice a day
+            </option>
+            <option value="Take medicine once a day">
+              Take medicine once a day
+            </option>
+            <option value="Take medicine for 3 days">
+              Take medicine for 3 days
+            </option>
+            <option value="Take medicine for 5 days">
+              Take medicine for 5 days
+            </option>
+            <option value="Take medicine for 7 days">
+              Take medicine for 7 days
+            </option>
+            <option value="Take medicine for 10 days">
+              Take medicine for 10 days
+            </option>
+            </select>
         </div>
         <div style={styles.formGroup}>
-          <label style={styles.label} htmlFor="medicine">
-            Medicine:
-          </label>
-          <textarea
+          <select
             style={styles.input}
             id="medicine"
             name="medicine"
             value={formData.medicine}
             onChange={handleChange}
-          />
+          >
+            <option value="">Select Medicine</option>
+            <option value="Paracetamol">Paracetamol</option>
+            <option value="Aspirin">Aspirin</option>
+            <option value="Ibuprofen">Ibuprofen</option>
+            <option value="Naproxen">Naproxen</option>
+            <option value="Diclofenac">Diclofenac</option>
+            <option value="Celecoxib">Celecoxib</option>
+            <option value="Acetaminophen">Acetaminophen</option>
+            <option value="Others">Others</option>
+          </select>
+
         </div>
         <button type="submit" style={styles.submitButton}>
           Create Prescription
